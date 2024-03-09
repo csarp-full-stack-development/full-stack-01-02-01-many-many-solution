6f48fd1bbb97a6230bc0de974f17506d3a5f8644
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-09 16:20:34 +0100

    Kész az osztályok tantárgyai

--- Kreta.Shared/Extensions/SchoolClassSubjectsExtension.cs
+++ Kreta.Shared/Extensions/SchoolClassSubjectsExtension.cs
@@ -5,27 +5,31 @@ namespace Kreta.Shared.Extensions
 {
     public static class SchoolClassSubjectsExtension
     {
-        public static SchoolClassSubjects ToModel(this SchoolClassSubjectsDto subject)
+        public static SchoolClassSubjects ToModel(this SchoolClassSubjectsDto dto)
         {
             return new SchoolClassSubjects
             {
-                Id= subject.Id,
-                SchoolClassId = subject.SchoolClassId,
-                SubjectId = subject.SubjectId,
-                NumberOfHours = subject.NumberOfHours,
-                IsTheHoursInOne= subject.IsTheHoursInOne,
+                Id= dto.Id,
+                SchoolClassId = dto.SchoolClassId,
+                SchoolClass = dto.SchoolClass,
+                SubjectId = dto.SubjectId,
+                Subject = dto.Subject,
+                NumberOfHours = dto.NumberOfHours,
+                IsTheHoursInOne= dto.IsTheHoursInOne,
             };
         }
 
-        public static SchoolClassSubjectsDto ToDto(this SchoolClassSubjects subject)
+        public static SchoolClassSubjectsDto ToDto(this SchoolClassSubjects model)
         {
             return new SchoolClassSubjectsDto
             {
-                Id = subject.Id,
-                SchoolClassId = subject.SchoolClassId,
-                SubjectId = subject.SubjectId,
-                NumberOfHours = subject.NumberOfHours,
-                IsTheHoursInOne = subject.IsTheHoursInOne,
+                Id = model.Id,
+                SchoolClassId = model.SchoolClassId,
+                SchoolClass = model.SchoolClass,
+                SubjectId = model.SubjectId,
+                Subject = model.Subject,
+                NumberOfHours = model.NumberOfHours,
+                IsTheHoursInOne = model.IsTheHoursInOne,
             };
         }
     }
